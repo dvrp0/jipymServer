2006b9593ff66d8f17a9b9f93b52c2d9634e157d
daveripper0020 <daveripper0020@gmail.com>
2023-03-15 03:06:07 +0900

    Pregeneration 및 fallback 색상 추가

--- src/api.py
+++ src/api.py
@@ -1,25 +1,20 @@
-import asyncio, io, re, requests
+import io, re, requests
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as diffusion
 from stability_sdk import client
 from typing import Optional
 from PIL import Image
 from const import *
 
-async def generate_text(toked: str) -> Optional[str]:
+def warm_up_model():
+    requests.post(GENERATE_URL, headers=GENERATE_HEADERS)
+
+def generate_text(toked: str) -> Optional[str]:
     GENERATE_DATA["inputs"] = f"{GENERATE_PROMPT}{toked}"
     response = requests.post(GENERATE_URL, json=GENERATE_DATA, headers=GENERATE_HEADERS)
-
-    if response.status_code == 503:
-        wait = float(response.json()["estimated_time"]) + GENERATE_EXTRA_WAIT_TIME
-        print(f"Loading model, wait for {wait}s")
-        await asyncio.sleep(wait)
-        print("Loading finished")
-        response = requests.post(GENERATE_URL, json=GENERATE_DATA, headers=GENERATE_HEADERS)
 
     if response.status_code == 200:
         essay = response.json()[0]["generated_text"][len(GENERATE_PROMPT):]
         splitted = re.split(r"((?:[.]|[?]|!){1}\s?)", essay)
-        print("Text generation done")
 
         return splitted[0].strip() if len(splitted) == 1 else "".join(splitted[:-1]).strip()
     else:
@@ -29,27 +24,17 @@ def translate(text: str) -> Optional[str]:
     url = f"{PAPAGO_URL}?source=ko&target=en&text={text}"
     response = requests.post(url, headers=PAPAGO_HEADERS);
 
-    if response.status_code == 200:
-        print("Translation done")
-
-        return response.json()["message"]["result"]["translatedText"]
-    else:
-        return None
+    return response.json()["message"]["result"]["translatedText"] if response.status_code == 200 else None
 
 def generate_image(text: str) -> Image:
     model = client.StabilityInference(key=STABILITY_KEY, engine=STABILITY_ENGINE)
     answers = model.generate(prompt=text)
-    requests.get(f"https://sbkrserver.deta.dev/generate_image")
 
-    for i, response in enumerate(answers):
+    for response in answers:
         for artifact in response.artifacts:
             if artifact.finish_reason == diffusion.FILTER:
-                print("Diffusion Inference request activated safety filter, fallback colors are used")
                 return None
-                # TODO: Fallback colors
             elif artifact.type == diffusion.ARTIFACT_IMAGE:
-                print("Text-to-Image generation done")
-
                 return Image.open(io.BytesIO(artifact.binary))
 
     return None
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from action import Action
 from api import *
 from datetime import datetime
 from deta import Deta
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pytz import timezone
 from typing import Optional
@@ -27,30 +27,34 @@ def get_random_input() -> str:
 def rgb_to_hex(r: int, g: int, b: int) -> str:
     return f"#{hex(r)[2:].zfill(2)}{hex(g)[2:].zfill(2)}{hex(b)[2:].zfill(2)}" #16진수 문자 0x 제거 후 0으로 채워서 6자리 헥스 코드로 변환
 
-async def generate_new_essay():
+def generate_new_essay():
     toked = get_random_input()
-    essay = await generate_text(toked)
+    essay = generate_text(toked)
 
     if essay:
         translation = translate(essay)
         image = generate_image(translation)
-        extracted, pixel_count = extcolors.extract_from_image(image)
-        colors = [rgb_to_hex(color[0][0], color[0][1], color[0][2]) for color in extracted[:2]]
+
+        if image:
+            extracted, pixel_count = extcolors.extract_from_image(image)
+            colors = [rgb_to_hex(color[0][0], color[0][1], color[0][2]) for color in extracted[:2]]
+        else:
+            random.shuffle(FALLBACK_COLORS)
+            colors = FALLBACK_COLORS[:2]
 
         db.put(data={"input": toked,
                      "body": essay[len(toked) + 1:],
                      "gradientFrom": colors[0],
                      "gradientTo": colors[1]},
                key=datetime.now(timezone("Asia/Seoul")).strftime("%Y%m%d"))
-        print("Successfully generated")
         requests.post(CLOUDFLARE_DEPLOY_URL)
-    else:
-        print("Error occurred during generation")
 
 @app.post("/__space/v0/actions")
-def post_actions(action: Action, background_tasks: BackgroundTasks):
-    if action.event.id == "generation":
-        background_tasks.add_task(generate_new_essay)
+def post_actions(action: Action):
+    if action.event.id == "pregeneration":
+        warm_up_model()
+    elif action.event.id == "generation":
+        generate_new_essay()
 
         return {"result": "Generation started"}
 
